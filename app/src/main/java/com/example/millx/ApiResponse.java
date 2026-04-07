1391b1d4fd55e08b8dcda588ac5a07eb7a56cb0b
Vijay <vijayjk182005@gmail.com>
2026-04-07 14:25:27 +0530

    test

--- app/src/main/java/com/example/millx/ApiResponse.java
+++
@@ -1,6 +0,0 @@
-package com.example.millx;
-
-public class ApiResponse {
-    public String status;
-    public String message;
-}
--- app/src/main/java/com/example/millx/User.java
+++
@@ -1,8 +0,0 @@
-package com.example.millx;
-
-public class User {
-    public int id;
-    public String name;
-    public String email;
-    public String role;
-}
